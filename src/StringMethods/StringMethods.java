e9095426cfc23db1b8e8ae31bd9460dc63bcd785
League <League@DESKTOP-A86R9LN>
2020-08-12 15:37:29 -0700

    String Methods

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -1,7 +1,9 @@
 package StringMethods;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
 
 /*
 Visit the JavaDocs for the String class to view everything you can do with a String.  
@@ -32,68 +34,113 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if (s1.length() > s2.length()) {
+			return s1;
+		} else {
+			return s2;
+		}
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if (s.contains("underscores")) {
+			s = s.replace(' ', '_');
+		}
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
+		String[] names = new String[] { s1, s2, s3 };
+		
+
 		return null;
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int sum = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				int currentNum = Integer.parseInt(s.charAt(i) + "");
+				sum += currentNum;
+			}
+		}
+
+		return sum;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int counter = 0;
+		int startIndex = 0;
+
+		while (s.indexOf(substring, startIndex) >= 0) {
+			counter++;
+			startIndex = s.indexOf(substring, startIndex) + 1;
+		}
+
+		return counter;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		String encrypted = Utilities.encrypt(s.getBytes(), (byte) key);
+		return encrypted;
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		String decrypted = Utilities.decrypt(s, (byte) key);
+		return decrypted;
 	}
-
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		String[] words = s.split(" ");
+		int counter = 0;
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].endsWith(substring)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		int firstIndex = s.indexOf(substring);
+		int lastIndex = s.lastIndexOf(substring);
+		return lastIndex - firstIndex - substring.length();
 	}
-
 
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
-		return true;
+		String str = s.replaceAll("[^a-zA-Z0-9]", "");
+		String reversedStr = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reversedStr = reversedStr + str.charAt(i);
+		}
+
+		int idk = str.compareToIgnoreCase(reversedStr);
+
+		if (idk == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
 }
 
 class Utilities {
